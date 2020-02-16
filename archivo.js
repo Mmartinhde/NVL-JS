413ea1351dc06bfcac434da17c75fe4e38d247c2
Maria Martin <maria.mhzd@gmail.com>
2020-02-16 20:20:34 +0100

    js Please enter the commit message for your changes. Lines starting

---
+++ archivo.js
@@ -0,0 +1,13 @@
+var nombre;
+var edad;
+var diassemanas;
+var valor gravedad;
+var numimpares;
+
+var nombre = "Maria Martin"
+var edad = 34
+var diassemanas = "Lunes, Martes, Miercoles, Jueves, Viernes"
+var valor gravedad = 9,81
+var numimpares = 1, 3, 5
+
+
